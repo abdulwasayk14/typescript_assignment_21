580e65f07f77dd5366b154d19b35de55042abaec
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 09:58:03 +0500

    Add files via upload

---
+++ Typescript-Object.js
@@ -0,0 +1,19 @@
+"use strict";
+const apple = {
+    name: "Apple",
+    color: "Red",
+    pricePerkg: 12.5
+};
+const orange = {
+    name: "Orange",
+    color: "Orange",
+    pricePerkg: 12.5
+};
+const banana = {
+    name: "Banana",
+    color: "Yellow",
+    pricePerkg: 2.5
+};
+console.log(apple);
+console.log(orange);
+console.log(banana);
---
+++ Typescript-Object.ts
@@ -0,0 +1,30 @@
+
+
+interface Fruit {
+
+    name: string;
+    color: string;
+    pricePerkg: number;
+}
+
+const apple: Fruit = {
+    name:  "Apple",
+    color:  "Red",
+    pricePerkg:  12.5
+}
+
+const orange: Fruit = {
+    name:  "Orange",
+    color:  "Orange",
+    pricePerkg: 12.5
+}
+
+const banana: Fruit = {
+    name: "Banana",
+    color: "Yellow",
+    pricePerkg:2.5
+}
+
+console.log (apple);
+console.log (orange);
+console.log (banana)
